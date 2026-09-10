3cec1cbabf95957ddcd35b4c9c9e8490130872c1
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-09-10 12:34:26 +0530

    feat: implement create message API

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { createConversation as createConversationService } from "../services/conversation.service.js";
+import { createConversation as createConversationService, createMessage as createMessageService } from "../services/conversation.service.js";
 import { AppError } from "../lib/error.js";
 
 export async function createConversation(req: Request, res: Response) {
@@ -10,4 +10,22 @@ export async function createConversation(req: Request, res: Response) {
     const conversation = await createConversationService(listingId, buyerId);
 
     return res.status(201).json({ conversation });
+}
+
+export async function createMessage(req: Request, res: Response) {
+    const senderId = req.userId;
+    const { conversationId } = req.params;
+    const { content } = req.body;
+
+    if (typeof conversationId !== "string") {
+        throw new AppError("Invalid conversation ID", 400);
+    }
+
+    if (!senderId) {
+        throw new AppError("Invalid sender Id", 400);
+    }
+
+    const message = await createMessageService(conversationId, senderId, content);
+
+    return res.status(200).json({ message });
 }
--- backend/src/validators/conversation.validator.ts
+++ backend/src/validators/conversation.validator.ts
@@ -3,3 +3,7 @@ import { z } from "zod";
 export const createConversationSchema = z.object({
     listingId: z.string().min(1),
 });
+
+export const createMessageSchema = z.object({
+  content: z.string().trim().min(1).max(1000),
+});
